3ea52f4dd53be5963123fc75e0a3cdcd3736fa58
yezhen <563739385@qq.com>
2020-05-20 18:17:04 +0800

    修改文案

--- 520/js/main.js
+++ 520/js/main.js
@@ -173,7 +173,7 @@ var config = function () {
 			color6: '#00ffff',
 			color7: '#fff'
 		},
-		dialogue: [{ type: 6, name: '冯强', txt: '快过年了，我们去放烟花吧！' }, { type: 2, name: '家丽', txt: '天还这么亮，现在放烟花也不好看。' }, { type: 2, name: '家丽', txt: '再说你那有烟花吗？' }, { type: 6, name: '冯强', txt: '我当然有烟花啦。' }, { type: 6, name: '冯强', txt: '你那有打火机吗？' }, { type: 2, name: '家丽', txt: '没有呀～' }, { type: 6, name: '冯强', txt: '那你是怎么点燃我的心的？' }, { type: 2, name: '家丽', txt: '。。。' }, { type: 2, name: '家丽', txt: '可是我的心还没有被你点燃呀。' }, { type: 6, name: '冯强', txt: '别慌，我去买个打火机先。' }, { type: 2, name: '家丽', txt: '打火机可点不燃我的心。' }, { type: 6, name: '冯强', txt: '天快黑了，我要为你点燃整片天空。' }],
+		dialogue: [{ type: 6, name: '冯强', txt: '520了，我们去放烟花吧！' }, { type: 2, name: '家丽', txt: '天还这么亮，现在放烟花也不好看。' }, { type: 2, name: '家丽', txt: '再说你那有烟花吗？' }, { type: 6, name: '冯强', txt: '我当然有烟花啦。' }, { type: 6, name: '冯强', txt: '你那有打火机吗？' }, { type: 2, name: '家丽', txt: '没有呀～' }, { type: 6, name: '冯强', txt: '那你是怎么点燃我的心的？' }, { type: 2, name: '家丽', txt: '。。。' }, { type: 2, name: '家丽', txt: '可是我的心还没有被你点燃呀。' }, { type: 6, name: '冯强', txt: '别慌，我去买个打火机先。' }, { type: 2, name: '家丽', txt: '打火机可点不燃我的心。' }, { type: 6, name: '冯强', txt: '天快黑了，我要为你点燃整片天空。' }],
 		// 阶段二
 		sunset: 8000,
 
